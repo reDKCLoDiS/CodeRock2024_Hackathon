17dde0d2ea2436a510aba1e410b286dd34615854
Alexey Kolegov <alexeykolegov.clodis@gmail.com>
2024-04-09 12:21:22 +0400

    Database of map is upgraded and dijkstra and py func synchronized.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ if __name__ == "__main__":
     data = open('data.json')
     graph = json.load(data)
     data.close()
-    for i in graph["Graph_details"]:
+    for i in graph["distance"]:
         print(f"{i['Graph_from']}, {i['Graph_to']}, {i['Length']}\n")
+        g.addEdge(i['Graph_from'], i['Graph_to'], i['Length'])
     #print(graph)
+    print('\n')
+    for i in graph["information"]:
+        print(graph["information"][i])
